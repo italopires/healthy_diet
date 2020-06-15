4483313e21fef3e1aedc79af11e90f5a086e6416
Italo Pires <italonapalm@gmail.com>
2020-06-15 20:45:18 -0300

    Added html template

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,7 +10,8 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require rails-ujs
-//= require activestorage
-//= require turbolinks
-//= require_tree .
+//= require jquery/jquery
+//= require jquery_ujs
+//= require bootstrap/bootstrap
+//= require propeller
+//= require_tree ./web

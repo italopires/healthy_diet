fc751bdb6050d90f2546d66bc32f103a6c7f9dee
Italo Pires <italonapalm@gmail.com>
2020-06-19 21:49:22 -0300

    Fixed initial date field from diet form

--- app/controllers/web/dashboards_controller.rb
+++ app/controllers/web/dashboards_controller.rb
@@ -1,7 +1,8 @@
 class Web::DashboardsController < ApplicationController
   def index
-    diet = current_user.diets.order(initial_date: :asc).first
-    weight_movements = diet.weight_movements.order(:reference_date)
+    diet = current_user.diets.order(initial_date: :desc).first
+    weight_movements = diet.weight_movements.where(reference_date: diet.initial_date..diet.final_date)
+                           .order(reference_date: :desc).limit(30)
     days = weight_movements.map { |meal| meal.reference_date.day  }
     weights = weight_movements.map { |weight_movement| weight_movement.weight }
     @data = {
